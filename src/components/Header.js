e988769e4c693141b6df2c9cc306333369d91c5c
shivendra <shivendrakeshari11@gmail.com>
2024-04-30 01:00:28 +0530

    Made changes

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header className="header-container">
+      <div className="header-content">
+        <h2 className="title">
+          <a href="/">Where in the world?</a>
+        </h2>
+        <p className="theme-changer">
+          <i className="fa-regular fa-moon" />
+          &nbsp;&nbsp;Dark Mode
+        </p>
+      </div>
+    </header>
+    </div>
+  )
+}
+
+export default Header
