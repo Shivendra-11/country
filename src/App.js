e7274857f1a7456330df25f0c97479a3490423eb
shivendra <shivendrakeshari11@gmail.com>
2024-04-30 02:21:24 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
+
 import Header from './components/Header';
 import './App.css';
+import Search from './components/Search';
+import SelectMenu from './components/SelectMenu';
+import CountryCard from './components/CountryCard';
+import CountryContainer from './components/CountryContainer';
 
 function App() {
   return (
     <>
     <Header/>
+    <main>
+    <div className='search-filter-container' > 
+    <Search/>
+    <SelectMenu/>
+      </div>
+<CountryContainer/>
+    </main>
+    <CountryContainer/>
+   
     </>
   );
 }
